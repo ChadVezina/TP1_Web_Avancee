3ede87d0eb9325bdc3544336763ef127cc956047
Chad Vezina <chadvezina@hotmail.com>
2025-05-22 18:23:35 -0400

    Modification access database

--- classes/Db.php
+++ classes/Db.php
@@ -1,7 +1,20 @@
 <?php
-    class Db extends PDO {
-        public function __construct() {
-            parent::__construct('mysql:host=localhost;dbname=myBlog;port=3307;charset=utf8', 'root', '');
+require_once dirname(__DIR__) . '/config/database.php';
+
+class Db extends PDO {
+    public function __construct() {
+        $dsn = "mysql:host=" . DB_HOST . 
+               ";dbname=" . DB_NAME . 
+               ";port=" . DB_PORT . 
+               ";charset=" . DB_CHARSET;
+        
+        try {
+            parent::__construct($dsn, DB_USER, DB_PASS);
             $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            // Log error but don't expose sensitive information
+            error_log("Database connection error: " . $e->getMessage());
+            die("Could not connect to the database.");
         }
     }
+}
